5cd8f868b775bf25c001d0be87ae3f97a6126b83
Jeanikt <jeanikkt@gmail.com>
2025-02-12 01:59:47 -0300

    criado novo arquivo jobFilter.js

---
+++ src/jobFilter.js
@@ -0,0 +1,19 @@
+function filterJobs(jobs, filters) {
+  return jobs.filter(job => {
+    if (filters.title && !job.title.toLowerCase().includes(filters.title.toLowerCase())) {
+      return false;
+    }
+    if (filters.company && !job.company.toLowerCase().includes(filters.company.toLowerCase())) {
+      return false;
+    }
+    if (filters.location && !job.location.toLowerCase().includes(filters.location.toLowerCase())) {
+      return false;
+    }
+    if (filters.source && job.source !== filters.source) {
+      return false;
+    }
+    return true;
+  });
+}
+
+module.exports = { filterJobs };
